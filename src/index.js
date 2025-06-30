67a8a0ab0c3b21eb2ba2a0090ef1621a94bde1ee
Rizone2k <email-cua-Rizone2k@gmail.com>
2025-06-30 15:42:26 +0700

    install template engine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const handlebars = require('express-handlebars');
+const express = require("express");
+const morgan = require('morgan')
+const app = express();
+const port = 3000;
+
+// http logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine('.hbs', handlebars.engine({
+  extname : ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join( __dirname,'resources\\views'));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Blog app listening on port ${port}`);
+});
