d5404090435e031734ab75290ef2f32207b75de4
Rizone2k <email-cua-Rizone2k@gmail.com>
2025-07-01 16:12:51 +0700

    Install bootstrap & SASS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-const path = require('path');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const handlebars = require("express-handlebars");
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // http logger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // template engine
-app.engine('.hbs', handlebars.engine({
-  extname : ".hbs"
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join( __dirname,'resources\\views'));
+app.engine(
+  ".hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
 
 app.get("/", (req, res) => {
   res.render("home");
